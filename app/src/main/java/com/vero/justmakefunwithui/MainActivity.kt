8c0c2efbcf6ab0709f403ab68cd24f43586eb63a
AndreVero <andriyveremiyenko@gmail.com>
2023-12-11 16:50:19 +0000

    Add gender picker UI

--- app/src/main/java/com/vero/justmakefunwithui/MainActivity.kt
+++ app/src/main/java/com/vero/justmakefunwithui/MainActivity.kt
@@ -7,8 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.vero.justmakefunwithui.components.baseclock.Clock
+import com.vero.justmakefunwithui.components.genderpicker.GenderPicker
 import com.vero.justmakefunwithui.ui.theme.JustMakeFunWithUITheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +19,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Clock(radius = 150.dp, modifier = Modifier.fillMaxSize())
+                    GenderPicker(onGenderClick = {})
                 }
             }
         }
